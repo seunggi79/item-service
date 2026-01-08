1d4ee510f426e5112291d26ba26b8479229ebc95
seunggi <hsg77689469.gamil.com>
2026-01-09 00:58:20 +0900

    Post/Redirect/Get

--- src/main/java/hello/item_service/web/basic/BasicItemController.java
+++ src/main/java/hello/item_service/web/basic/BasicItemController.java
@@ -67,10 +67,18 @@ public class BasicItemController {
      * @ModelAttribute 자체 생략 가능
      * model.addAttribute(item) 자동 추가
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+    /**
+     * PRG - Post/Redirect/Get
+     */
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
